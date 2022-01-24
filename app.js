b06d7c7ae40c7afe47a0e8fe04db093308b6d55a
srijit patra <srijitpatra123@gmail.com>
2022-01-24 17:40:19 +0530

    updated email mechanism

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const path = require("path");
 
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const routes = require("./routes");
+
+const port = process.env.PORT || 5000;
 
 const app =express();
 
@@ -12,14 +15,14 @@ app.set('views', 'views');
 //catch all favicon get request and return it to 204(no content status code)
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.use(express.static(path.join(__dirname , 'public')));
 
 app.use(routes);
 
-app.listen(process.env.PORT || 5000, (err)=> {
+app.listen(port, (err)=> {
     if(err)
     console.log(err);
-    console.log("server connected at "+ 5000);
+    console.log("server connected at "+ port);
 })
--- controllers.js
+++ controllers.js
@@ -1,16 +1,35 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const user = process.env.user;
+const clientId = process.env.clientId;
+const clientSecret = process.env.clientSecret;
+const redirectUri = process.env.redirectUri;
+const refreshToken = process.env.refreshToken;
 
 exports.getMail = (req , res, next) =>{
     res.render('form' , {message: ''});
 };
 
-exports.postMail = (req, res, next) =>{
+exports.postMail = async (req, res, next) =>{
     const name = req.body.name;
     const company = req.body.company;
     const email = req.body.email;
     const phone = req.body.phone;
     const message = req.body.message;
     console.log(req.body);
+
+    const oauth2Client = new OAuth2(
+        clientId,clientSecret,redirectUri
+    );
+    
+    oauth2Client.setCredentials({
+        refresh_token: refreshToken
+    });
+    // const accessToken = oauth2Client.getAccessToken();
+
     const output = ` 
         <p> A New Contact Form Was Created By You.</p>
         <h3> Contact Details </h3>
@@ -23,25 +42,42 @@ exports.postMail = (req, res, next) =>{
         <h3>Message</h3>
         <p>${message}</p>
     `;
+   
     const transporter = nodemailer.createTransport({
       service : "gmail",
       auth: {
-        user: "itssrijit099@gmail.com",
-        pass: "Uzumakinaruto100"
+        type: "OAuth2",
+        user: user,
+        clientId: clientId,
+        clientSecret: clientId,
+        refreshToken: refreshToken,
+        accessToken:  "ya29.A0ARrdaM_lfklwwQ4SzDvmW-2iOTL2Xi8ge06lLEI8MkrkcA1_847wwLGYliIkFGQ_I9e3Ab70ChyytzvunASLX_foIk0xzQlP_6yOiD1ig6M58uBtYNUKv3efFPwF5YA9rvhzowt70644Oyh8KKpsctuREvxN"
+      },
+      tls:{
+        rejectUnauthorized:false
       }
     });  
     
     // send mail with defined transport object
-    let info = transporter.sendMail({
-        from: 'node-mailer', // sender address
-        to: "srijitpatra123@gmailcom", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "HEY there!!", // plain text body
-        html: output, // html body
-        });
+    const mailOptions = {
+      ffrom: `'Its Srijit ' <${user}>`, // sender address
+      to: email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "HEY there!!", // plain text body
+      html: output, // html body
+    };
 
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));    
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      console.log('error occured');
+    }
+
+    else {
+      console.log(info);
+      console.log("success");
+    }
+  });
 
     res.render("form", {message : "Email has been Sent"});
 }
